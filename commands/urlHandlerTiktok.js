56884801474488259813ada42e735fa37c3203d9
korikashi <pedarkasy@gmail.com>
2025-11-10 17:04:33 -0500

    Fix: remove 'vt' subdomain from modified Tiktok URL

--- commands/urlHandlerTiktok.js
+++ commands/urlHandlerTiktok.js
@@ -1,11 +1,11 @@
 async function handleUrlTiktok(message) {
     if (message.author.bot) return; // ignora los mensajes mandados por bots
 
-    const patron = /https?:\/\/(?:[a-z]+\.)?tiktok\.com\/@[^/]+\/video\/\d+|https?:\/\/vt\.tiktok\.com\/[^\/?]+/gi; // expresion regular para detectar enlaces de tiktok
+     const patron = /https?:\/\/(?:[a-z]+\.)?tiktok\.com\/@[^/]+\/video\/\d+|https?:\/\/vt\.tiktok\.com\/[^\/?]+/gi; // expresion regular para detectar enlaces de tiktok
     const match = message.content.match(patron); // busca coincidencias con la URL de tiktok
 
     if (match) {
-        const modifiedURL = match[0].replace('tiktok', 'tnktok'); // modifica el dominio de la URL encontrada
+        const modifiedURL = match[0].replace(/^https?:\/\/(?:vt\.)?tiktok\.com/, 'https://tnktok.com');  // modifica el dominio de la URL encontrada
 
         // envia un mensaje con la URL modificada  
         await message.reply({content: modifiedURL });
